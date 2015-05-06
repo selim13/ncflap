c489fcfa62f39ecd64e9dcdf60a8560a275af8af
Dmitry Seleznyov <selim013@gmail.com>
2015-05-06 22:12:57 +0300

    Display main window dimensions

--- win.c
+++ win.c
@@ -1,9 +1,32 @@
-#include <ncursesw/ncurses.h>
+#include <ncurses.h>
+
+#include "win.h"
+
+void win_drawborder(WINDOW *win)
+{
+        box(win, ACS_VLINE, ACS_HLINE);
+
+#ifdef WIN_DEBUG
+        int x, y, yy, i;
+        getmaxyx(win, y, x);
+
+        mvwprintw(win, y - 1, x / 2, "%d", x);
+
+        i = 0;
+        yy = y;
+        while (yy > 0) {
+                mvwprintw(win, y / 2 - i, 0, "%d", yy % 10 );
+                yy /= 10;
+                i++;
+        }
+
+#endif
+}
 
 WINDOW *win_create(int height, int width, int y, int x)
 {
 	WINDOW *win = newwin(height, width, y, x);
-	box(win, ACS_VLINE, ACS_HLINE);
+        win_drawborder(win);
 	wrefresh(win);
 
 	return win;
@@ -13,7 +36,7 @@ void win_resize(WINDOW *win, int height, int width)
 {
 	wresize(win, height, width);
 	wclear(win);
-	box(win, ACS_VLINE, ACS_HLINE);
+        win_drawborder(win);
 	wrefresh(win);
 }
 
@@ -22,13 +45,13 @@ void win_change(WINDOW *win, int height, int width, int y, int x)
 	wresize(win, height, width);
 	mvwin(win, y, x);
 	wclear(win);
-	box(win, ACS_VLINE, ACS_HLINE);
+        win_drawborder(win);
 	wrefresh(win);
 }
 
 void win_clear(WINDOW *win)
 {
         werase(win);
-	box(win, ACS_VLINE, ACS_HLINE);
+        win_drawborder(win);
 	wrefresh(win);
 }
--- win.h
+++ win.h
@@ -3,6 +3,8 @@
 
 #include <ncurses.h>
 
+#define WIN_DEBUG 1
+
 WINDOW *win_create(int height, int width, int y, int x);
 void win_resize(WINDOW *win, int height, int width);
 void win_change(WINDOW *win, int height, int width, int y, int x);
